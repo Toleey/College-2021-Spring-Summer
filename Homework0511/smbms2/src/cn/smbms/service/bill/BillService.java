ce9163b57136505291e848a0a480dadbcb894df4
Toleey <Toleey@outlook.com>
2021-05-12 09:21:33 +0800

    Update 0512

---
+++ Homework0511/smbms2/src/cn/smbms/service/bill/BillService.java
@@ -0,0 +1,18 @@
+package cn.smbms.service.bill;
+
+
+
+import cn.smbms.pojo.Bill;
+
+import java.util.List;
+
+public interface BillService {
+    public List<Bill> findBillListByProductNameAndProviderAndIsPayment(
+            String productName,Integer providerId,Integer isPayment, Integer fromLineNum, Integer toLineNum);
+    public Integer findCountBillListByProductNameAndProviderAndIsPayment(
+            String productName,Integer providerId,Integer isPayment);
+    public Bill findViewBillById(Integer id);
+    public boolean addBill(Bill bill);
+    public boolean updateBill(Bill bill);
+    public boolean deleteBill(Integer id);
+}
